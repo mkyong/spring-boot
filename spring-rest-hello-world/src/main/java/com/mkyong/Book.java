7d89dbfb2d6a5c4201ad01d783ac56664496522c
mkyong <mkyong2002@gmail.com>
2019-02-19 16:30:50 +0800

    spring rest hello world

---
+++ spring-rest-hello-world/src/main/java/com/mkyong/Book.java
@@ -0,0 +1,69 @@
+package com.mkyong;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+    private String author;
+    private BigDecimal price;
+
+    // avoid this "No default constructor for entity"
+    public Book() {
+    }
+
+    public Book(String name, String author, BigDecimal price) {
+        this.name = name;
+        this.author = author;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", author='" + author + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
---
+++ spring-rest-hello-world/src/main/java/com/mkyong/error/CustomGlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.mkyong.error;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@ControllerAdvice
+public class CustomGlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    // Let Spring handle the exception, we just override the status code
+    @ExceptionHandler(BookNotFoundException.class)
+    public void springHandleNotFound(HttpServletResponse response) throws IOException {
+        response.sendError(HttpStatus.NOT_FOUND.value());
+    }
+
+    @ExceptionHandler(BookUnSupportedFieldPatchException.class)
+    public void springUnSupportedFieldPatch(HttpServletResponse response) throws IOException {
+        response.sendError(HttpStatus.METHOD_NOT_ALLOWED.value());
+    }
+
+}
