66e568e5837ce2cb741f0af1e3e78737689b758f
mkyong <mkyong2002@gmail.com>
2019-03-21 15:49:35 +0800

    init

---
+++ spring-data-jpa/src/main/java/com/mkyong/StartApplication.java
@@ -0,0 +1,42 @@
+package com.mkyong;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartApplication implements CommandLineRunner {
+
+    private static final Logger log = LoggerFactory.getLogger(StartApplication.class);
+
+    @Autowired
+    private BookRepository repository;
+
+    public static void main(String[] args) {
+        SpringApplication.run(StartApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) {
+
+        log.info("StartApplication...");
+
+        repository.save(new Book("Java"));
+        repository.save(new Book("Node"));
+        repository.save(new Book("Python"));
+
+        System.out.println("\nfindAll()");
+        repository.findAll().forEach(x -> System.out.println(x));
+
+        System.out.println("\nfindById(1L)");
+        repository.findById(1l).ifPresent(x -> System.out.println(x));
+
+        System.out.println("\nfindByName('Node')");
+        repository.findByName("Node").forEach(x -> System.out.println(x));
+
+    }
+
+}
