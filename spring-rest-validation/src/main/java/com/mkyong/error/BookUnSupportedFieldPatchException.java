e74d94532af456821b111160538a5b719026d929
Mauricio ID <mauricioid23@gmail.com>
2021-02-14 11:38:24 -0300

    Added required serialVersionUID fields

--- spring-rest-validation/src/main/java/com/mkyong/error/BookUnSupportedFieldPatchException.java
+++ spring-rest-validation/src/main/java/com/mkyong/error/BookUnSupportedFieldPatchException.java
@@ -4,6 +4,8 @@ import java.util.Set;
 
 public class BookUnSupportedFieldPatchException extends RuntimeException {
 
+    private static final long serialVersionUID = 1L;
+
     public BookUnSupportedFieldPatchException(Set<String> keys) {
         super("Field " + keys.toString() + " update is not allow.");
     }
