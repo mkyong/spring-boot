342b087202c988eb80062579cfc137b2b4a84dbf
rintu2016 <124765051+rintu2016@users.noreply.github.com>
2023-06-09 16:51:47 +0530

    Update BookRepository.java

--- spring-data-jpa/src/main/java/com/mkyong/BookRepository.java
+++ spring-data-jpa/src/main/java/com/mkyong/BookRepository.java
@@ -7,6 +7,8 @@ import java.util.List;
 public interface BookRepository extends CrudRepository<Book, Long> {
 
     List<Book> findByName(String name);
+    
+    public <User> fetchByName(String name);
 
     public <User> fetchById(int id);
 
