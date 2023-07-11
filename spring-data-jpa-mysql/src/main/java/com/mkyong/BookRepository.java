b4b1d1500fad6b62295554ad45e733d6aafbe46a
ritnu2016 <psatheesh77@gmail.com>
2023-07-11 15:53:58 +0530

    find functionality changes

--- spring-data-jpa-mysql/src/main/java/com/mkyong/BookRepository.java
+++ spring-data-jpa-mysql/src/main/java/com/mkyong/BookRepository.java
@@ -7,7 +7,9 @@ import java.util.List;
 public interface BookRepository extends CrudRepository<Book, Long> {
 
     List<Book> findByName(String name);
-    
+
     List<Book> findById(int id);
+
+    List<Book> findByPrice(int price);
 
 }
