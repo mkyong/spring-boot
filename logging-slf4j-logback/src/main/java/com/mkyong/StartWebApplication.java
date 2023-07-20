73a8091c063e3e1c8c70668f5a53593ba50bc121
ritnu2016 <psatheesh77@gmail.com>
2023-07-20 21:49:44 +0530

    Revert "User functionality changes"
    This reverts commit 4b444a765906af2027f9fd90e8b7c9da98eb1a08.

--- logging-slf4j-logback/src/main/java/com/mkyong/StartWebApplication.java
+++ logging-slf4j-logback/src/main/java/com/mkyong/StartWebApplication.java
@@ -4,17 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StartWebApplication implements CommandLineRunner{
-
-@Autowired
-UserRepository repo;
+public class StartWebApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StartWebApplication.class, args);
-
-		User u = new User(1,"rintu",45000);
-		repo.deleteById(1);
 	}
 
 }
-
