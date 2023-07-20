4b444a765906af2027f9fd90e8b7c9da98eb1a08
ritnu2016 <psatheesh77@gmail.com>
2023-07-20 21:37:12 +0530

    User functionality changes

--- logging-slf4j-logback/src/main/java/com/mkyong/StartWebApplication.java
+++ logging-slf4j-logback/src/main/java/com/mkyong/StartWebApplication.java
@@ -4,10 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StartWebApplication {
+public class StartWebApplication implements CommandLineRunner{
+
+@Autowired
+UserRepository repo;
 
 	public static void main(String[] args) {
 		SpringApplication.run(StartWebApplication.class, args);
+
+		User u = new User(1,"rintu",45000);
+		repo.deleteById(1);
 	}
 
 }
+
