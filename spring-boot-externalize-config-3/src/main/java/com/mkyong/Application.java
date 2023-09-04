a858b0f5072c422974f82369d16fa2aa3244f835
mkyong <mkyong2002@gmail.com>
2023-09-04 12:25:29 +0800

    update properties

--- spring-boot-externalize-config-3/src/main/java/com/mkyong/Application.java
+++ spring-boot-externalize-config-3/src/main/java/com/mkyong/Application.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
