ac71a0e0e1fd0266824de7f99cdc42f6948d63f2
ritnu2016 <psatheesh77@gmail.com>
2024-03-09 17:08:25 +0530

    un and pwd functionality changes

--- spring-data-jpa-postgresql/src/main/java/com/mkyong/BookRepository.java
+++ spring-data-jpa-postgresql/src/main/java/com/mkyong/BookRepository.java
@@ -7,5 +7,7 @@ import java.util.List;
 public interface BookRepository extends CrudRepository<Book, Long> {
 
     List<Book> findByName(String name);
+    
+    public User findByUnAndPwd(String un,String pwd);
 
 }
