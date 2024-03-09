7d6dbfebdfed6e838424a6199cd55c930a1467c3
ritnu2016 <psatheesh77@gmail.com>
2024-03-09 17:22:22 +0530

    evert "Update BookRepository.java"
    This reverts commit c018accc18469c5d37b09855db5cfc6315f5dee0.

--- spring-data-jpa-postgresql/src/main/java/com/mkyong/BookRepository.java
+++ spring-data-jpa-postgresql/src/main/java/com/mkyong/BookRepository.java
@@ -7,8 +7,6 @@ import java.util.List;
 public interface BookRepository extends CrudRepository<Book, Long> {
 
     List<Book> findByName(String name);
-
-    public User findById(int id);
     
     public User findByUnAndPwd(String un,String pwd);
 
